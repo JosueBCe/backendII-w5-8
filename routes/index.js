75eb4cf86d953068fce08c9a4829a0f3e9938fb8
Josue Benjamin <josuebcenturion@gmail.com>
2023-02-28 13:33:12 -0300

    swagger documentation for the API

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,14 @@
 const routes = require('express').Router();
+const router = require('express').Router();
+
 
 // Route to display Name
-routes.use('/', require("./displayName") );
+// routes.use('/', require("./displayName") );
+
+router.use('/', require('./swagger'));
 
 // Route to display the contacts from DB
 routes.use('/contacts', require("./contacts") );
-
 
 
 module.exports = routes;
