a5c5c1aef03671baf8f02fa2503cb24d92e052f9
Josue Benjamin <josuebcenturion@gmail.com>
2023-03-03 13:21:49 -0300

    testing doc api

--- swagger.js
+++ swagger.js
@@ -2,20 +2,20 @@ const swaggerAutogen = require('swagger-autogen')();
 
 const doc = {
   info: {
-    title: 'Backend Course API',
-    description: 'Backend Personal Assignment API'
+    title: 'My API Doc',
+    description: 'Backend II',
   },
-  host: 'backend-dev-1joz.onrender.com',
-  schemes: ['https']
+  host: 'localhost:8080',
+  schemes: ['http'],
 };
 
 const outputFile = './swagger.json';
 const endpointsFiles = ['./routes/index.js'];
 
-// generate swagger.json
+//generate swagger.json
 swaggerAutogen(outputFile, endpointsFiles, doc);
 
-// Run server after it gets generated
-// swaggerAutogen(outputFile, endpointsFiles, doc).then(async () => {
-//   await import('./index.js');
-// });
+//  // Run server after it gets generated
+//  swaggerAutogen(outputFile, endpointsFiles, doc).then(async () => {
+//    await import('./routes/index.js');
+//  });
